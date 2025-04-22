15c627706ac0bb5d91caaa848785b34a9f3daa15
konradkrawczyk <konrad.krawczyk@mytracko.pl>
2025-04-22 14:18:18 +0200

    IN-70 Implemented Crashlytics

--- README.md
+++ README.md
@@ -11,7 +11,23 @@ Invenza is a simple app, which allows you to create your team, create groups of 
 
 ## Getting Started
 
+### Firebase
+App uses firebase, so you have to create project in Firebase.
+Also you will need to install [Firebase](https://firebase.google.com/docs/cli?authuser=6#setup_update_cli) and [Flutterfire](https://firebase.google.com/docs/flutter/setup?platform=ios#install-cli-tools)
+To login to Firebase just run
+```
+firebase login
+```
+And to configure app project, run
+```
+flutterfire configure
+```
+
+### Mobile app
+
 To initialize project run this command
 ```
 flutter clean && flutter pub get && dart run build_runner build
 ```
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:app/app/app.dart';
 import 'package:app/di.dart';
+import 'package:app/firebase_options.dart';
 import 'package:app/interceptors/setup_interceptors.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
@@ -8,12 +11,27 @@ import 'package:path_provider/path_provider.dart';
 
 Future<void> main() async{
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+
+  PlatformDispatcher.instance.onError = (error, stack){
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+
   HydratedBloc.storage = await HydratedStorage.build(
     storageDirectory: kIsWeb
         ? HydratedStorageDirectory.web
         : HydratedStorageDirectory((await getTemporaryDirectory()).path),
   );
+
   configureDependencies();
+
   setupInterceptors();
+
   runApp(const App());
 }
