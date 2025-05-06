b569e0b8ab0b90ddbfc33312364abb44a7f5af02
konradkrawczyk <konrad.krawczyk@mytracko.pl>
2025-05-06 09:18:50 +0200

    IN-121 Preperred for local development

--- lib/modules/ip_module.dart
+++ lib/modules/ip_module.dart
@@ -1,3 +1,17 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+
 abstract class IpModule {
-  static String get ipAddress => 'https://kkonrad02.pl:443';
+  static String ipAddress(){
+    if(kReleaseMode){
+      return 'https://kkonrad02.pl:443';
+    }
+
+    if(Platform.isAndroid){
+      return 'http://10.0.2.2:443';
+    }else{
+      return 'http://localhost:443';
+    }
+  }
 }
