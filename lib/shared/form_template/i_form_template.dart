44b22b04b6471e3d1a26ef354df00b1c93820a96
konradkrawczyk <konrad.krawczyk@mytracko.pl>
2025-05-11 11:39:26 +0200

    IN-150 Expanded task description field

--- lib/shared/form_template/i_form_template.dart
+++ lib/shared/form_template/i_form_template.dart
@@ -1,7 +1,9 @@
 export 'models/i_form_widget.dart';
+export 'widgets/i_form_checkbox.dart';
 export 'widgets/i_form_checkbox_group/i_form_checkbox_group.dart';
 export 'widgets/i_form_checkbox_group/i_form_options.dart';
 export 'widgets/i_form_date_time.dart';
+export 'widgets/i_form_expanded_text_field.dart';
 export 'widgets/i_form_loading_widget.dart';
 export 'widgets/i_form_secure_field.dart';
 export 'widgets/i_form_template.dart';
---
+++ lib/shared/form_template/widgets/i_form_expanded_text_field.dart
@@ -0,0 +1,55 @@
+import 'package:app/extensions/text_extension.dart';
+import 'package:app/shared/form_template/models/i_form_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
+
+class IFormExpandedTextField extends IFormStatelessWidget {
+  const IFormExpandedTextField({
+    required this.name,
+    this.label,
+    this.placeholder,
+    this.actionNext = true,
+    this.validators = const [],
+    this.initialValue,
+    this.keyboardType,
+    this.disabled = false,
+    super.key,
+  });
+
+  final String name;
+  final String? label;
+  final String? placeholder;
+  final bool actionNext;
+  final List<FormFieldValidator<String>> validators;
+  final String? initialValue;
+  final TextInputType? keyboardType;
+  final bool disabled;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        if (label != null)
+          Text(
+            label!,
+            style: context.bodyMedium,
+          ),
+        FormBuilderTextField(
+          name: name,
+          initialValue: initialValue,
+          enabled: !disabled,
+          maxLines: 5,
+          decoration: InputDecoration(
+            hintText: placeholder,
+          ),
+          textInputAction:
+          actionNext ? TextInputAction.next : TextInputAction.done,
+          keyboardType: keyboardType,
+          validator: FormBuilderValidators.compose(validators),
+        ),
+      ],
+    );
+  }
+}
