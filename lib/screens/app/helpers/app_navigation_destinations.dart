5b6dde62d3779138a0c4490ae6c11e31a00550cf
konradkrawczyk <konrad.krawczyk@mytracko.pl>
2025-05-21 21:20:04 +0200

    MA-201 Restored default navBar

--- lib/screens/app/helpers/app_navigation_destinations.dart
+++ lib/screens/app/helpers/app_navigation_destinations.dart
@@ -1,4 +1,5 @@
 import 'package:app/app/routing/app_router.gr.dart';
+import 'package:app/extensions/app_localizations.dart';
 import 'package:app/extensions/color_extension.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
@@ -11,7 +12,7 @@ class AppNavigationDestinations {
   List<NavigationDestination> getDestinationList() => [
         NavigationDestination(
           icon: Icon(Icons.task_outlined, color: context.secondary),
-          label: '',
+          label: context.l10n.nav_bar_tasks,
           selectedIcon: Icon(Icons.task, color: context.primary),
         ),
         NavigationDestination(
@@ -19,13 +20,13 @@ class AppNavigationDestinations {
             Icons.supervised_user_circle_outlined,
             color: context.secondary,
           ),
-          label: '',
+          label: context.l10n.nav_bar_team,
           selectedIcon:
               Icon(Icons.supervised_user_circle, color: context.primary),
         ),
         NavigationDestination(
           icon: Icon(Icons.settings_outlined, color: context.secondary),
-          label: '',
+          label: context.l10n.nav_bar_settings,
           selectedIcon: Icon(Icons.settings, color: context.primary),
         ),
       ];
